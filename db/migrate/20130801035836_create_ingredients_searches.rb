2071f70a4d9abc547e8e79227dc336f4c33f176e
Amy Hua <foramyhua@gmail.com>
2013-07-31 21:07:50 -0700

    Migrated Ingredients_search table

--- db/migrate/20130801035836_create_ingredients_searches.rb
+++ db/migrate/20130801035836_create_ingredients_searches.rb
@@ -1,8 +1,12 @@
 class CreateIngredientsSearches < ActiveRecord::Migration
   def change
     create_table :ingredients_searches do |t|
+    t.references :ingredient
+    t.references :search
+    
 
-      t.timestamps
     end
+    add_index :ingredients_searches, :ingredient_id
+    add_index :ingredients_searches, :search_id
   end
 end
