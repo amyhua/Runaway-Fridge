3a51bd714458f6423ae9fa2fcfd162f5bac8eb88
Amy Hua <foramyhua@gmail.com>
2013-07-31 21:28:34 -0700

    Correct IngredientsSearch model, correct IngredientsSearch migrations

--- app/models/ingredients_search.rb
+++ app/models/ingredients_search.rb
@@ -1,5 +1,6 @@
 class IngredientsSearch < ActiveRecord::Base
-  belongs_to :ingredients
-  belongs_to :searches
+  belongs_to :ingredient
+  belongs_to :search
+  accepts_nested_attributes_for :ingredient, :reject_if => :all_blank
   
 end
--- db/migrate/20130801035836_create_ingredients_searches.rb
+++ db/migrate/20130801035836_create_ingredients_searches.rb
@@ -1,12 +1,13 @@
 class CreateIngredientsSearches < ActiveRecord::Migration
   def change
     create_table :ingredients_searches do |t|
-    t.references :ingredient
-    t.references :search
-    
+      t.references :ingredient
+      t.references :search
+      
 
     end
     add_index :ingredients_searches, :ingredient_id
     add_index :ingredients_searches, :search_id
   end
 end
+
